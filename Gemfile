ad58a77bdde7a60f04a531e08690fab251e446eb
ogarocious <awesomeogar@gmail.com>
2022-08-25 20:05:01 -0500

    update CSS and event submit form

--- Gemfile
+++ Gemfile
@@ -14,7 +14,7 @@ gem 'sass-rails', '>= 6'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.7'
 # Use Redis adapter to run Action Cable in production
-# gem 'redis', '~> 4.0'
+gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
 gem 'bcrypt', '~> 3.1.7'
 
@@ -86,3 +86,5 @@ group :development, :test do
 end
 
 gem "importmap-rails", "~> 1.1"
+
+gem "hotwire-livereload", "~> 1.2", :group => :development
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,6 +17,12 @@ Rails.application.configure do
   # Enable server timing
   config.server_timing = true
 
+
+  config.hotwire_livereload.listen_paths << Rails.root.join("app/assets/stylesheets")
+  config.hotwire_livereload.force_reload_paths << Rails.root.join("app/assets/stylesheets")
+  config.hotwire_livereload.force_reload_paths << Rails.root.join("app/javascript")
+
+
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join("tmp/caching-dev.txt").exist?
@@ -34,7 +40,7 @@ Rails.application.configure do
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  config.active_storage.service = :amazon
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
